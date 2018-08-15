9e8851a3a1a42b0bafba39d656878f3068b37a63
SUJEET <sujeetkmrswain@gmail.com>
2018-08-15 18:37:35 +0530

    another small changes

---
+++ currency-exchange-servuce/src/test/java/com/devappspring/microservices/currencyexchangeservuce/CurrencyExchangeServuceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.devappspring.microservices.currencyexchangeservuce;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CurrencyExchangeServuceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
